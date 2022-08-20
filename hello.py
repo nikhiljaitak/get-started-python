2bfbd4d6c402677d7777cbf2d8188ce5cd609ed5
nikhiljaitak <niks4u93@gmail.com>
2022-08-20 15:28:17 +0530

    print added

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ app = Flask(__name__, static_url_path='')
 db_name = 'mydb'
 client = None
 db = None
-
+print('done')
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.getenv('VCAP_SERVICES'))
     print('Found VCAP_SERVICES')
